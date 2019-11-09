cda4645c180b2aefa581d04dc7fa1ee8995f50b2
tianlijun <977081003@qq.com>
2019-11-09 12:44:36 +0800

    修改

--- src/components/huibox/boxli.vue
+++ src/components/huibox/boxli.vue
@@ -50,8 +50,6 @@ activated() {} // 如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
 <style  scoped>
-ul li {
-  background: red !important
-}
+
 
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,19 @@
 module.exports = {
-    baseUrl: './',
-    assetsDir: 'static',
+  baseUrl: './',
+  assetsDir: 'static',
 
-    // devServer: {
-    //     proxy: {
-    //         '/api':{
-    //             target:'http://jsonplaceholder.typicode.com',
-    //             changeOrigin:true,
-    //             pathRewrite:{
-    //                 '/api':''
-    //             }
-    //         }
-    //     }
-    // }
-    productionSourceMap: false,
+  // devServer: {
+  //     proxy: {
+  //         '/api':{
+  //             target:'http://jsonplaceholder.typicode.com',
+  //             changeOrigin:true,
+  //             pathRewrite:{
+  //                 '/api':''
+  //             }
+  //         }
+  //     }
+  // }
+  productionSourceMap: false,
 
-    publicPath: './',
-
-    css: {
-      modules: true
-    }
+  publicPath: './'
 }
