0a1ba91c78d8ad485651052d6e7b4595d76db9ae
12956 <1295684363@qq.com>
2019-11-24 13:37:48 +0800

    导出与Avue

--- src/components/huiindex/indextuanka.vue
+++ src/components/huiindex/indextuanka.vue
@@ -23,43 +23,48 @@
           <div class="conments">
             <div class="yingbut">
               <el-form-item label="营销活动">
-                <el-select v-model="form.region" class="sex">
-                  <el-option label="参加营销活动" value="hex"></el-option>
-                  <el-option label="不参加营销活动" value="shenji"></el-option>
+                <el-select v-model="form.region">
+                  <el-option
+                          v-for="item in regionOptions"
+                          :key="item.value"
+                          :label="item.label"
+                          :value="item.value"
+                          @click.native="OpenRegion(item.value)"
+                  >
+                  </el-option>
                 </el-select>
               </el-form-item>
             </div>
             <el-form-item label="会籍类型">
-              <el-select v-model="form.region3">
-                <el-option label="H1002 - 闪电狼会员1月0元" value="yin"></el-option>
-                <el-option label="L210 - 连锁金卡12月2398" value="yxbj"></el-option>
-                <el-option label="L216 - 连锁金卡1年赠卡" value="yin"></el-option>
-                <el-option label="L293 - 连锁金卡3月0" value="yxbj"></el-option>
-                <el-option label="M80471 - 单店100次卡  3000" value="yin"></el-option>
-                <el-option label="N10265 - 单店12月1+X卡 0" value="yxbj"></el-option>
-                <el-option label="R1001 - 24城单店12月100次卡4000" value="yin"></el-option>
-                <el-option label="R1003 - 蓝色空间老卡1月0元" value="yxbj"></el-option>
-                <el-option label="R1004 - 蓝色空间老卡1月10次0元" value="yin"></el-option>
-                <el-option label="T1060 - 连锁金卡598续费2598" value="yxbj"></el-option>
+              <el-select v-model="typeHui">
+                <el-option
+                        v-for="item in typeHuiOptions"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                        @click.native="OpenType(item.value)"
+                >
+                </el-option>
               </el-select>
+
             </el-form-item>
 
           </div>
 
           <el-form-item label="销售类型">
-            <el-select v-model="form.region" class="sex">
-              <el-option label="1-新合同" value="hex"></el-option>
-              <el-option label="2-升级" value="shenji"></el-option>
-              <el-option label="3-续会" value="xuhj"></el-option>
+            <el-select v-model="form.region" class="sex" >
+              <el-option label="1-新合同" value="0" @click.native="OpenRegion(0)"></el-option>
+              <el-option label="2-升级" value="1" @click.native="OpenRegion(1)"></el-option>
+              <el-option label="3-续会" value="3"></el-option>
             </el-select>
           </el-form-item>
 
           <div class='divbin'>
             <el-form-item label="有效期">
-              <input type="text" class="input_box">
+              <input type="text" class="input_box" v-model="typeHuiList.periodDate">
             </el-form-item>
             <el-form-item label="赠送天数">
-              <input type="text" class="input_box">
+              <input type="text" class="input_box" v-model="typeHuiList.giveDate">
             </el-form-item>
 
           </div>
@@ -71,10 +76,10 @@
 
           <div class='divbin'>
             <el-form-item label="指导价">
-              <input type="text" class="input_box">
+              <input type="text" class="input_box" v-model="typeHuiList.guidingPrice">
             </el-form-item>
             <el-form-item label="总价">
-              <input type="text" class="input_box">
+              <input type="text" class="input_box" v-model="typeHuiList.totalPrice">
             </el-form-item>
 
           </div>
@@ -100,9 +105,21 @@
         </el-table>
         <span class="titex">可使用门店</span>
         <el-table :data="mendata" border style="width: 58%">
-          <el-table-column prop="menx" label="门店" width="106"></el-table-column>
-          <el-table-column prop="miao" label="描述" width="120"></el-table-column>
-          <el-table-column prop="zixuan" label="自选门店" width="120"></el-table-column>
+          <el-table-column
+                  type="selection"
+                  width="55">
+          </el-table-column>
+          <el-table-column
+                  prop="menx"
+                  label="门店"
+                  width="145">
+          </el-table-column>
+          <el-table-column
+                  prop="miao"
+                  label="描述"
+                  width="147">
+          </el-table-column>
+
         </el-table>
       </div>
 
@@ -148,7 +165,46 @@
 import Dialog from '../common/Dialog'
 // 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 // 例如：import 《组件名称》 from '《组件路径》';
-
+const optionList = [
+  {value: '1', label: 'H1002 - 闪电狼会员1月0元'},
+  {value: '2', label: 'L210 - 连锁金卡12月2398'},
+  {value: '3', label: 'L216 - 连锁金卡1年赠卡'},
+  {value: '4', label: 'L293 - 连锁金卡3月0'},
+  {value: '5', label: 'M80471 - 单店100次卡  3000'},
+  {value: '6', label: 'N10265 - 单店12月1+X卡 0'},
+  {value: '7', label: 'R1001 - 24城单店12月100次卡4000'},
+  {value: '8', label: 'R1003 - 蓝色空间老卡1月0元'},
+  {value: '9', label: 'R1004 - 蓝色空间老卡1月10次0元'},
+  {value: '10', label: 'T1060 - 连锁金卡598续费2598'},
+];
+const optionType = [
+          {value: '1',label: 'D0888 - 通用3月980不含24城游泳和天誉'},
+          {value: '2',label: 'L201 - 连锁VIP12月卡38888'},
+          {value: '3',label: 'L202 - 连锁金卡12月2598每年598续'},
+          {value: '4',label: 'L204 - 连锁VIP12月卡3888'},
+          {value: '5',label: 'L207 - 连锁金卡12月2298'},
+          {value: '6',label: 'L208 - 连锁金卡5年3888'},
+          {value: '7',label: 'L209 - 连锁金卡5年4888'},
+          {value: '8',label: 'L211 - 连锁金卡12月2598'},
+          {value: '9',label: 'L212 - 连锁金卡2月900'},
+          {value: '10',label: 'L213 - 连锁金卡3月1200'},
+          {value: '11',label: 'L214 - 连锁金卡6月1500'},
+          {value: '12',label: 'L215 - 连锁金卡5年5888'},
+          {value: '13',label: 'L217 - 连锁金卡4年3888'},
+          {value: '14',label: 'L218 - 连锁金卡6月2198'},
+          {value: '15',label: 'L219 - 连锁金卡3月1298'},
+          {value: '16',label: 'L220 - 连锁金卡1月900'},
+          {value: '17',label: 'L221 - 连锁金卡1月680'},
+          {value: '18',label: 'L227 - 连锁金卡3年3888'},
+          {value: '19',label: 'L231 - 连锁金卡三人同行家庭卡3年8400（儿童只游泳）'},
+          {value: '20',label: 'L234 - 连锁金卡2月1000'},
+          {value: '21',label: 'L260 - 连锁金卡6月1538'},
+          {value: '22',label: 'L280 - 连锁金卡3月1000'},
+        ];
+const regionList = [
+          {value: '0',label: '参加营销活动'},
+          {value: '1',label: '不参加营销活动'},
+        ];
 export default {
 
   name: 'indextuanka',
@@ -164,6 +220,22 @@ export default {
   data () {
     // 这里存放数据
     return {
+      checked: true,
+      //下拉框
+      typeHui: '',
+      typeHuiOptions: optionList,
+      regionOptions: regionList,
+      typeHuiList:{
+        //有效期
+        periodDate :'',
+        //指导价
+        guidingPrice : '',
+        //总价
+        totalPrice:'',
+        //赠送天数
+        giveDate :'',
+      },
+
       dialog: {
         show: false
       },
@@ -190,7 +262,6 @@ export default {
         mun: '66666'
 
       }],
-
       bianta: [{
         biu: '110',
         nbm: '11111111111111111'
@@ -199,7 +270,7 @@ export default {
         menx: 'ssssss',
         miao: '2222222222222',
         zixuan: 'xxxxxxxxxxx'
-      }]
+      }],
     }
   },
   // 监听属性 类似于data概念
@@ -209,6 +280,56 @@ export default {
 
   // 方法集合
   methods: {
+
+    OpenType(value){
+      if(value == 1){
+        console.log(value);
+        this.mendata = [{
+          menx: '111111',
+          miao: '111111',
+          zixuan: '111111'
+        }];
+      }else if(value == 2){
+        this.mendata=[{
+          menx: '2222222',
+          miao: '222222',
+          zixuan: '22222'
+        }]
+      }else {
+        this.mendata=[{
+          menx: '测试数据',
+          miao: '222222',
+          zixuan: '测试数据'
+        }]
+      }
+      //生成随机数
+      this.typeHuiList.periodDate =  Math.floor(Math.random()*10+1)+'年';
+      this.typeHuiList.giveDate = Math.floor(Math.random()*36+1)+'天';
+      this.typeHuiList.totalPrice=this.typeHuiList.guidingPrice = Math.floor(Math.random()*3000);
+      const end = new Date();
+      const start = new Date();
+      this.value1 = [end,start]
+
+    },
+    //判断是否参加营销活动
+    OpenRegion(value){
+     if(value == '0'){
+       console.log(value);
+       this.typeHuiOptions = optionType
+       this.typeHui = '请选择'
+       this.mendata = []
+       this.value1 = []
+       this.typeHuiList = {}
+     }else if(value == '1'){
+       console.log(value);
+       this.typeHuiOptions = optionList
+       this.typeHui = '请选择'
+       this.mendata = [],
+       this.typeHuiList = {}
+       this.value1 = []
+     }
+     //导入
+    },
     // print () {
     //   this.$print(this.$refs.print) // 使用
     // },
@@ -229,7 +350,6 @@ export default {
   },
   // 生命周期 - 创建完成（可以访问当前this实例）
   created () {
-
   },
   // 生命周期 - 挂载完成（可以访问DOM元素）
   mounted () {
@@ -243,6 +363,7 @@ export default {
   destroyed () { }, // 生命周期 - 销毁完成
   activated () { } // 如果页面有keep-alive缓存功能，这个函数会触发
 }
+
 </script>
 <style scoped>
 .boxst {
