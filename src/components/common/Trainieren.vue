e9836ba0ce1e43fc38f5e1c6c79636b7ab9aa994
wangwei <wangweixxt@dingtalk.com>
2019-12-21 09:39:08 +0800

    12.21.1修改

---
+++ src/components/common/Trainieren.vue
@@ -0,0 +1,96 @@
+<!-- vue快捷创建组件 -->
+<template>
+  <div class='app'>
+    <el-dialog :title="drillen.title" :visible.sync="drillen.show" tooltip-effect="dark" width="50%" :close-on-click-modal="false" :close-on-press-escape="false" :modal-append-to-body="false">
+      <el-form :inline="true" :model="form" class="demo-form-inline">
+        <el-row :gutter="24">
+          <el-col :span="5">
+            <el-input type="text" v-model="form.name" placeholder="会籍姓名"></el-input>
+          </el-col>
+          <el-button type="primary" @click="shousuo">搜索</el-button>
+        </el-row>
+
+        <template>
+          <el-radio-group v-model="radio">
+            <el-radio :label="3">刘刚</el-radio>
+            <el-radio :label="6">刘晓军</el-radio>
+            <el-radio :label="9">养狗狗</el-radio>
+          </el-radio-group>
+        </template>
+
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="drillen.show = false">取 消</el-button>
+        <el-button type="primary" @click="drillen.show = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+// 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+// 例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  name: 'Dialog',
+  props: {
+    drillen: Object,
+    contract: Array
+  },
+  // import引入的组件需要注入到对象中才能使用
+  components: {
+
+  },
+  data () {
+    // 这里存放数据
+    return {
+      emit: '',
+      form: {
+        name: '',
+      },
+      radio1: '上海',
+      radio: ''
+    }
+  },
+  // 监听属性 类似于data概念
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 方法集合
+  methods: {
+    handleSelectionChange (val) {
+      this.emit = val[0]
+    },
+    shousuo () {
+      console.log(23)
+    }
+  },
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created () {
+
+  },
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted () {
+
+  },
+  beforeCreate () { }, // 生命周期 - 创建之前
+  beforeMount () { }, // 生命周期 - 挂载之前
+  beforeUpdate () { }, // 生命周期 - 更新之前
+  updated () { }, // 生命周期 - 更新之后
+  beforeDestroy () { }, // 生命周期 - 销毁之前
+  destroyed () { }, // 生命周期 - 销毁完成
+  activated () { } // 如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style scoped>
+@import './../../assets/css/table.css';
+.el-dialog__body {
+    padding: 10px 20px !important;
+    color: #606266;
+    font-size: 14px;
+    word-break: break-all;
+}
+.el-radio-group{
+  margin-top: 25px
+}
+</style>
